771889f5fdf7a1f0bac2ddd2aa319913dc179860
suraj vishwakarma <smartcraze354@gmail.com>
2025-04-26 18:41:07 +0000

    solved z index erorr  search bar in navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,16 +4,16 @@ import IncidentForm from './IncidentForm';
 
 function Navbar() {
   return (
-    <div className="w-full px-4 flex justify-center md:flex-row items-center gap-4 mt-3 p-2 backdrop-blur-md  shadow-lg">
-    
+    <div className="w-full px-4 flex justify-between items-center gap-4 p-2 bg-black relative z-10">
       <div className="text-2xl font-extrabold text-amber-500">
-        <span className="text-gray-100">AI</span> Dashboard
+        <span className="text-white">AI</span> Dashboard
       </div>
-
       
+      <div className="flex-grow max-w-xl mx-4 z-50">
         <SearchBar />
-        <IncidentForm />
-     
+      </div>
+      
+      <IncidentForm />
     </div>
   );
 }
